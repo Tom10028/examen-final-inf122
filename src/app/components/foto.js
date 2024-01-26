7c36c1a678d06dede4a6de82fcc42bf7e181c620
Tom10028 <155449444+Tom10028@users.noreply.github.com>
2024-01-26 13:18:39 +0000

    Aignación de Pokemon

---
+++ src/app/components/foto.js
@@ -0,0 +1,12 @@
+"use client"
+import React from "react";
+import style from "./foto.module.css";
+import { useState, useEffect } from "react";
+import Image from "next/image";    
+function Foto(){
+    return(
+
+        
+    );
+}
+export default Foto;
